757cbb6168266e4ba2da78c16e0e01051ac16e84
Alexander-Lucas <Alexander.Lucas.Cohort225@gmail.com>
2022-08-10 15:07:02 -0700

    Alexander's updated room 1 & 5-7

--- text-adventure.js
+++ text-adventure.js
@@ -21,3 +21,105 @@ class Room {
 }
 
 room1 = new Room(1, 'The cool room', 'It\'s really cool here', 'Big');
+
+
+
+const room1 = {
+	id: 1,
+	name: 'The cold room',
+	description: 'a cold room, filled with an eerie calm. You can see a gate to to the west and an opening to the north and east.',
+	dimensions: 'big',
+	exits: {
+		north: {
+			id: n, 
+			exitsTo: 'room6', 
+			isSecret: false,
+			isBlocked: false
+		},
+		east: {
+			id: e,
+			exitsTo: 'room3',
+			isSecret: false,
+			isBlocked: false
+		},
+		west: {
+			id: w,
+			exitsTo: 'room2',
+			isSecret: false,
+			isBlocked: true
+		}
+	},
+	content: ['torch'],
+	visited: false
+};
+
+//Room 5 the coal room. 
+const room5 = {
+	id: 5,
+	name: 'The coal room',
+	description: 'a small soot ridden room that pulls a cough from your lungs. You can see an opening to the north.',
+	dimensions: 'small',
+	exits: {
+		north: {
+			id: n,
+			exitsTo: 'room3',
+			isSecret: false,
+			isBlocked: false
+		}
+	},
+	content: ['pickaxe'],
+	visited: false
+};
+//Room 6 the waterfall room. 
+const room6 = {
+	id: 6,
+	name: 'The waterfall room',
+	description: 'a room filled with damp air, coming from a waterfall at the north side of the room. You can see an opening to the south.',
+	dimensions: 'narrow',
+	exits: {
+		north: {
+			id: n,
+			exitsTo: 'room7',
+			isSecret: true,
+			isBlocked: false
+		},
+		south: {
+			id: s,
+			exitsTo: 'room1',
+			isSecret: false,
+			isBlocked: false
+		}
+	},
+	content: [],
+	visited: false
+};
+
+//Room 7 the caved in room. 
+const room7 = {
+	id: 7,
+	name: 'The caved in room',
+	description: 'a room strewn with rubble from the rotted supports to the west. You can see an opening to the east and south.',
+	dimensions: 'medium',
+	exits: {
+		east: {
+			id: e,
+			exitsTo: 'room10',
+			isSecret: false,
+			isBlocked: false
+		},
+		south: {
+			id: s,
+			exitsTo: 'room6',
+			isSecret: false,
+			isBlocked: false
+		},
+		west: {
+			id: n,
+			exitsTo: 'room8',
+			isSecret: true,
+			isBlocked: true
+		},
+	},
+	content: [],
+	visited: false
+};
